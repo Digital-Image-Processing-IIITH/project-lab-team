fadc0bb690f625058fa3c83a693dff6917338e03
Aryamaan Jain <aryamaanjain@gmail.com>
2020-11-02 21:53:59 +0530

    started csg_tree

--- csg_tree.py
+++ csg_tree.py
@@ -1,2 +1,135 @@
-class CSGTree:
+class NodeContent():
+	"""docstring for NodeContent"""
+	def __init__(self, shape):
+		self.shapes = [shape]
+
+
+class Node():
+	"""docstring for Node"""
+	def __init__(self, shapes):
+		self.parent = None
+		self.content = NodeContent(shapes)
+		self.node_tag = -1
+
+
+class OpNode(Node):
+	"""docstring for OpNode"""
+	def __init__(self):
+		self.child_left = None
+		self.child_right = None
+		self.op_type = None
+
+
+class LeafNode(Node):
+	"""docstring for LeffNode"""
+	def __init__(self):
+		self.copies = None
+		self.animation = None
+
+
+class Tree():
+	"""docstring for Tree"""
+	def __init__(self, nodes=[], leaves=[]):
+		self.node_id = 0
+		self.root = None
+		self.nodes = nodes
+		self.leaves = leaves
+
+
+def InitTree():
+	return Tree()
+
+
+def AddShape(tree, shape);
+	node = LeafNode()
+	node.node_tag = get_node_id(tree)
+	noceC = NodeContent(shape)
+	node.content = nodeC
+	AddNode(tree, node)
+	return node
+
+
+def LeafNode* CopyNode(tree, a);
+	copy = LeafNode()
+	copy.content = NodeContent(a.content.shapes)
+	a.copies.append(copy)
+	return copy	
+
+
+def Sum(tree, a, b, update = true):
+	pass
+
+
+def Union(Tree* tree, Node* a, Node* b, bool update = true):
+	pass
+
+
+def Difference(tree, a, b, update = true):
+	pass
+
+
+def Intersection(tree, a, b, update = true):
+	pass
+
+
+def XOR(tree, a, b, update = true):
+	pass
+
+
+def PlaceInShape(tree, a, b, update = true):
+	pass
+
+
     
+def UpdateLeafNode(tree, a, anim, delta, update = true):
+	pass
+
+
+def UpdateLeafNode(tree, a, anim, current_time, incr, total_dur, update = true):
+	pass
+
+
+def UpdateLeafNode(tree, a, anim, frame, update = true):
+	pass
+
+
+def UpdateContent(tree, a):
+	pass
+
+
+def UpdateTree(tree):
+	pass
+
+
+def PropagateContent(tree, a):
+	pass
+
+
+    
+def UpdateOpNode(tree, a):
+	pass
+
+
+    
+def AddNode(tree, node):
+	pass
+
+
+def AddNode(tree, node):
+	pass
+
+
+    
+def FindNode(tree, shape):
+	pass
+
+
+    
+def BuildResult(tree, shapes, a, b):
+	pass
+
+
+    
+def get_node_id(tree):
+	pass
+
